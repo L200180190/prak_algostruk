e644a2581fc41e61406c6ad36895aefa7e51be2b
L200180190 <48145366+L200180190@users.noreply.github.com>
2020-03-17 19:19:44 +0700

    Update modul_2.py

--- modul2/modul_2.py
+++ modul2/modul_2.py
@@ -1,1 +1,51 @@
+# nomer3
+
+class Manusia(object):
+    """class 'Manusia' dengan inisiasi 'nama'"""
+    keadaan = "lapar"
+    def __init__(self, nama):
+        self.nama = nama
+    def ucapkanSalam(self):
+        print("Salam, namaku ",self.nama)
+    def makan(self, s):
+        print("Saya baru saja makan ", s)
+        self.keadaan = 'kenyang'
+    def olahraga(self, k):
+        print("Saya baru saja latihan ", k)
+        self.keadaan = 'lapar'
+    def mengalikanDenganDua(self, n):
+        return n * 2
+
+class Mahasiswa(Manusia):
+    """Class Mahasiswa yang dibangun dari class Manusia."""
+    def __init__(self):
+        """Metode inisiasi ini menutupi metode inisiasi di class Manusia"""
+        self.nama = str(input("Masukkan Nama : "))
+        self.NIM = str(input("Masukkan NIM : ")) 
+        self.kotaTinggal= str(input("Masukkan Kota Tinggal : "))
+        self.uangSaku= int(input("Masukkan Jumlah Uang Saku : "))
+    def __str__(self):
+        s = "Nama : " + self.nama + ", NIM : " + str(self.NIM) \
+            + ". Tinggal di " + self.kotaTinggal \
+            + ". Uang Saku Rp " + str(self.uangSaku) \
+            + " tiap bulannya."
+        return s
+    def ambilNama(self):
+        return self.nama
+    def ambilNIM(self):
+        return self.NIM
+    def ambilUangSaku(self):
+        return self.uangSaku
+    def makan(self, s):
+        """Metode ini menutupi metode 'makan'-nya class manusia.
+           Mahasiswa kalau makan sambil belajar."""
+        print("Saya baru saja makan", s, "Sambil belajar.")
+        self.keadaan = 'kenyang'
+
+    def ambilKotaTinggal(self):
+        return self.kotaTinggal
+    def perbaruiKotaTinggal(self, ubah):
+        self.kotaTinggal = ubah
+    def tambahUangSaku(self, tambah):
+        self.uangSaku += tambah
 
