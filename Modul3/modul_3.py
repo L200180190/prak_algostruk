69141d24b6d598487dfd45797c9a9877bd21b42e
L200180190 <48145366+L200180190@users.noreply.github.com>
2020-03-18 23:05:42 +0700

    Update modul_3.py

--- Modul3/modul_3.py
+++ Modul3/modul_3.py
@@ -1,1 +1,249 @@
+#Nomer 1
+a = [[6,5],[8,9]]
+b = [[14,1],[7,4]]
+c = [[11,3,"y"],[12,5,9]]
+d = [[3,4],[2,4],[1,5]]
+e = [[1,2,3],[4,5,6]]
+f = [[3,4,5],[4,5,6],[8,9,10]]
+
+def cekKonsis(n):
+    x = len(n[0])
+    y = type(n[0][0])
+    z = 0
+    a = True
+    for i in range (len(n)):
+        for j in range (len(n[i])):
+            c = type(n[i][j])
+            if (c!=y):
+                a = False
+                break
+        if (len(n[i]) == x):
+            z+=1
+        
+    if(z == len(n) and a==True):
+        print("matriks konsisten")
+    else:
+        print("matrik tidak konsisten")
+
+def cekInt(n):
+    x = 0
+    y = 0
+    for i in n:
+        for j in i:
+            y+=1
+            if (str(j).isdigit()==False):
+                print("tidak semua isi matriks adalah angka")
+                break
+            else:
+                x+=1
+    if(x==y):
+        print("semua isi matriks adalah angka")
+
+def ordo(n):
+    x,y = 0,0
+    for i in range(len(n)):
+        x+=1
+        y = len(n[i])
+    print("mempunyai ordo "+str(x)+"x"+str(y))
+
+def jumlah(n,m):
+    x,y = 0,0
+    for i in range(len(n)):
+        x+=1
+        y = len(n[i])
+    xy = [[0 for j in range(x)] for i in range(y)]    
+    z = 0
+    if(len(n)==len(m)):
+        for i in range(len(n)):
+            if(len(n[i]) == len(m[i])):
+                z+=1
+    if(z==len(n) and z==len(m)):
+        print("ukuran sama")
+        for i in range(len(n)):
+            for j in range(len(n[i])):
+                xy[i][j] = n[i][j] + m[i][j]
+        print(xy)
+    else:
+        print("ukuran beda")
+
+def kali(n,m):
+    aa = 0
+    x,y = 0,0
+    for i in range(len(n)):
+        x+=1
+        y = len(n[i])
+    v,w = 0,0
+    for i in range(len(m)):
+        v+=1
+        w = len(m[i])
+    if(y==v):
+        print("bisa dikalikan")
+        vwxy = [[0 for j in range(w)] for i in range(x)]
+        print(vwxy)
+        for i in range(len(n)):
+            for j in range(len(m[0])):
+                for k in range(len(m)):
+                    vwxy[i][j] += n[i][k] * m[k][j]
+        print(vwxy)      
+    else:
+        print("tidak memenuhi syarat")
+
+def hitungD(A, total=0):
+    x = len(A[0])
+    z = 0
+    for i in range(len(A)):
+        if (len(A[i]) == x):
+           z+=1
+    if(z == len(A)):
+        if(x==len(A)):
+            indices = list(range(len(A)))
+            if len(A) == 2 and len(A[0]) == 2:
+                val = A[0][0] * A[1][1] - A[1][0] * A[0][1]
+                return val
+            for fc in indices: 
+                As = A 
+                As = As[1:] 
+                height = len(As) 
+                for i in range(height): 
+                    As[i] = As[i][0:fc] + As[i][fc+1:] 
+                sign = (-1) ** (fc % 2) 
+                sub_det = determHitung(As)
+                total += sign * A[0][fc] * sub_det
+        else:
+            return "tidak bisa dihitung determinan, bukan matrix bujursangkar"
+    else:
+        return "tidak bisa dihitung determinan, bukan matrix bujursangkar"
+    return total
+
+#Nomer 2
+def buatNol(n,m=None):
+    if(m==None):
+        m=n
+    print("membuat matriks 0 dengan ordo "+str(n)+"x"+str(m))
+    print([[0 for j in range(m)] for i in range(n)])
+
+def buatIdentitas(n):
+    print("membuat matriks identitas dengan ordo"+str(n)+"x"+str(n))
+    print([[1 if j==i else 0 for j in range(n)] for i in range(n)])
+    
+#Nomer 3
+class Node: 
+    def __init__(self, data): 
+        self.data = data 
+        self.next = None
+
+class LinkedList(): 
+    def __init__(self): 
+        self.head = None
+
+    def addF(self, new_data): 
+        new_node = Node(new_data) 
+        new_node.next = self.head 
+        self.head = new_node
+
+    def addE(self, data):
+        if (self.head == None):
+            self.head = Node(data)
+        else:
+            current = self.head
+            while (current.next != None):
+                current = current.next
+            current.next = Node(data)
+
+    def insert(self,data,pos):
+        node = Node(data)
+        if not self.head:
+            self.head = node
+        elif pos==0:
+            node.next = self.head
+            self.head = node
+        else:
+            prev = None
+            current = self.head
+            current_pos = 0
+            while(current_pos < pos) and current.next:
+                prev = current
+                current = current.next
+                current_pos +=1
+            node.next = prev.next
+            prev.next = node
+
+    def deleteNode(self, position): 
+        if self.head == None: 
+            return 
+        temp = self.head 
+        if position == 0: 
+            self.head = temp.next
+            temp = None
+            return 
+        for i in range(position ):
+            prev = temp
+            temp = temp.next
+            if temp is None: 
+                break
+        if temp is None: 
+            return 
+        if temp.next is None: 
+            return 
+        prev.next = temp.next
+        temp= None 
+
+    def search(self, x): 
+        current = self.head 
+        while current != None: 
+            if current.data == x: 
+                return "True"
+            current = current.next
+        return "False"
+
+    def display(self):
+        current = self.head
+        while current is not None:
+            print(current.data, end = ' ')
+            current = current.next   
+
+#Nomer 4
+class Node: 
+    def __init__(self, data): 
+        self.data = data 
+        self.prev = None
+
+class DoublyLinkedList: 
+    def __init__(self): 
+        self.head = None
+
+    def pertama(self, new_data):
+        print("menambah pada awal : ", new_data)
+        new_node = Node(new_data) 
+        new_node.next = self.head 
+        if self.head is not None: 
+            self.head.prev = new_node 
+        self.head = new_node 
+
+    def terakhir(self, new_data):
+        print("menambah pada akhir : ", new_data)
+        new_node = Node(new_data) 
+        new_node.next = None
+        if self.head is None: 
+            new_node.prev = None
+            self.head = new_node 
+            return 
+        last = self.head 
+        while(last.next is not None): 
+            last = last.next
+        last.next = new_node 
+        new_node.prev = last 
+        return
+
+    def cetakList(self, node): 
+        print("\nDari Depan :")
+        while(node is not None): 
+            print(" % d" %(node.data))
+            last = node 
+            node = node.next
+        print("\nDari Belakang :")
+        while(last is not None): 
+            print(" % d" %(last.data)) 
+            last = last.prev 
+            
 
